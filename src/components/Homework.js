da4b28d5870d5b8e8d4c6ac47602b6b27a3ae41a
ajayp <ajayplepton@gmail.com>
2025-08-09 14:46:54 +0530

    third commit

---
+++ src/components/Homework.js
@@ -0,0 +1,25 @@
+// let a = 15;
+// let b = 35;
+// let c = 55;
+
+// var average ;
+// average = ((a+b+c)/3);
+// console.log("average is :" , average)
+
+
+let p = 620;
+let q = 680;
+let r = 760;
+
+var largest_number;
+ if (p>q){
+    if(p>r){
+        console.log("p is the largest number", p)
+    }
+ } else if (q>r){
+    console.log("q is the largest number", q)
+
+ }
+ else {
+    console.log("r is the largest number", r)
+ }
